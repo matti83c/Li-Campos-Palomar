ee3711a39cf149ee8f6846932ed68a0c0011e592
Mateo Campos <112992933+matti83c@users.noreply.github.com>
2022-10-10 22:49:13 +0800

    added js file, some buttons, some css
    added css to the navigation buttons on ALL pages
    added a javascript file with some code in it for question generation
    added some buttons to album page

---
+++ js/script.js
@@ -0,0 +1,26 @@
+var alval = 0;
+
+function setNextQuestion() {
+    showQuestion(alval);
+}
+
+function showQuestion(alval) {
+    //if statements for alval
+}
+
+function selectAnswer() {
+
+}
+
+
+const questions = [
+    {
+        question: 'Placeholder',
+        answers: [
+            { text: 'placeholder', alval: 1},
+            { text: 'placeholder2', alval: 2}, //note: i have aboslutely no idea if this code works but it *should* be structured like this.
+            { text: 'placeholder3', alval: 3},
+            { text: 'placeholder4', alval: 4}
+        ]
+    }
+]
