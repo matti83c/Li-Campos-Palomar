a1a65fd58ee38143d10ebe360fdbc5e4bbe54f5e
Mateo Campos <112992933+matti83c@users.noreply.github.com>
2022-10-17 22:58:15 +0800

    began work on question system
    added ids to most things in output, fixed javascript to display questions and output (though it's all placeholders for now).

--- js/script.js
+++ js/script.js
@@ -1,26 +1,45 @@
 var alval = 0;
+var chooser = document.getElementById("chooser");
+var title = document.getElementById("title");
+var artist = document.getElementById("artist");
+var slength = document.getElementById("length");
+var tlength = document.getElementById("tlength");
+var release = document.getElementById("release");
+var genre = document.getElementById("genre");
 
-function setNextQuestion() {
-    showQuestion(alval);
+//look there's definitely a better way to do this but the only javascript program ive made before this was hello world
+function onClick1() {
+    alval += 1;
+    console.log("alval: " + alval);
+    showNextQuestion(alval);
 }
 
-function showQuestion(alval) {
-    //if statements for alval
+function onClick100() {
+    alval += 100;
+    console.log("alval: " + alval);
+    showNextQuestion(alval);
 }
 
-function selectAnswer() {
 
+function showNextQuestion(qval){
+    if(qval == 001)(document.getElementById("question").innerHTML = "I'm a new question!");
+    else if(qval == 002)(document.getElementById("question").innerHTML = "I'm a newer question!");
+    else if(qval == 003)(document.getElementById("question").innerHTML = "I'm a newest question!");
+    else if(qval == 143)(document.getElementById("question").innerHTML = "What's up!");
+
+    output(qval); 
 }
 
+function output(oval){
 
-const questions = [
-    {
-        question: 'Placeholder',
-        answers: [
-            { text: 'placeholder', alval: 1},
-            { text: 'placeholder2', alval: 2}, //note: i have aboslutely no idea if this code works but it *should* be structured like this.
-            { text: 'placeholder3', alval: 3},
-            { text: 'placeholder4', alval: 4}
-        ]
+    if(oval == 10){
+        chooser.remove();
+        title.innerHTML = "Hiiiiii";
+        artist.innerHTML = "Hiiiiii";
+        slength.innerHTML = "Hiiiiii";
+        tlength.innerHTML = "Hiiiiii";
+        release.innerHTML = "Hiiiiii";
+        genre.innerHTML = "Hiiiiii";
     }
-]
+
+}
